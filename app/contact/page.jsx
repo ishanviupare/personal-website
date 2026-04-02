da7a6d7bd250e1aa71685bbc7e93cff6317e7c54
Ishanvi Upare <ishanviupare@gmail.com>
2026-04-02 15:27:47 -0500

    color changing nav bar + contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,29 @@
+import { getDigestForWellKnownError } from 'next/dist/server/app-render/create-error-handler'
 import React from 'react'
+import Link from "next/link";
+
 
 const contact = () => {
   return (
-    <div>contact</div>
+    <div className = "h-screen bg-[#c6b0ad] p-20">
+      <h1 className = "text-6xl font-serif text-[#5a4a46]"> Contacts... </h1>
+
+      <div className = "flex flex-col items-center gap-12 p-20">
+        <div className = "flex justify-center gap-20">
+          <a href = "mailto:ishanviupare@gmail.com">
+            <img src = "/mail.png" className = "w-25"/>
+          </a>
+          <a href = "https://www.linkedin.com/in/ishanvi-upare-091910344/" target = "_blank">
+            <img src = "/Linkedin.png" className = "w-25"/>
+          </a>
+          <a href = "https://github.com/ishanviupare" target = "_blank">
+            <img src = "/Github.png" className = "w-25"/>
+          </a>
+        </div>
+        <img src = "flower.png" className = "w-200"/>
+      </div>
+
+    </div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,10 @@ import Link from "next/link";
 export default function Home() {
   return (
 
-    <div className = "h-screen w-screen flex bg-[#313628]">
+    <div className = "h-screen flex items-center justify-center bg-[#A5AE9E]">
 
-      <div className = "flex h-full w-full">
-
-        <div className = "w-3/4 bg-[#A5AE9E] flex flex-col items-center justify-center gap-11">
+        <div className = "flex flex-col items-center gap-11">
+          
           <h1 className = "text-8xl font-serif text-[#313628]">
             Ishanvi Upare 
           </h1>
@@ -16,15 +15,6 @@ export default function Home() {
           <img src = "_3.jpg" className = "w-155 h-155"/>
         </div>
 
-        <div className = "w-1/4 bg-[#313628] flex flex-col p-20 pl-22">
-          <div className = "flex flex-col gap-15 text-5xl font-serif text-[#A5AE9E]">
-            <Link href="/about">About</Link>
-            <Link href="/contact">Contact</Link>
-            <Link href="/projects">Projects</Link>
-          </div>
-        </div>
-
-      </div>
     </div>
   );
 };
